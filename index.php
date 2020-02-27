65818fb325423ee3157485ec0899f603739d7179
Bruno Henrique Costa <55752438+bhpcosta@users.noreply.github.com>
2020-02-27 18:55:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="theme-color" content="#ffffff"/>
 	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
 	<title>Home | Compliancer</title>
