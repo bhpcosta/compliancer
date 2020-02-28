6cb0c3dca2601458757a9e726d475516e96a4773
Bruno Henrique Costa <55752438+bhpcosta@users.noreply.github.com>
2020-02-28 00:33:07 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ if (('share' in navigator || isPWA.matches) && pwaMenu) {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js').then((registration) => {
+    navigator.serviceWorker.register('sw.js', { scope: '/' }).then((registration) => {
       console.log('SW registered: ', registration);
     }).catch((registrationError) => {
       console.log('SW registration failed: ', registrationError);
