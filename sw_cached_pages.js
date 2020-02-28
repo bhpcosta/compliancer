bd2a37410267cc62f4c69d58747959fe5a8bf8b6
Bruno Henrique Costa <55752438+bhpcosta@users.noreply.github.com>
2020-02-28 11:41:39 -0300

    Update sw_cached_pages.js

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -42,6 +42,15 @@ const cacheAssets = [
   '/img/work5.JPG',
   '/img/work6.JPG',
   '/js/main.js',
+  '/images/icons/icon-128x128.png',
+  '/images/icons/icon-144x144.png',
+  '/images/icons/icon-152x152.png',
+  '/images/icons/icon-192x192.png',
+  '/images/icons/icon-384x384.png',
+  '/images/icons/icon-512x512.png',
+  '/images/icons/icon-72x72.png',
+  '/images/icons/icon-96x96.png',
+  './css/style.css',
 ];
 
 // Call Install Event
