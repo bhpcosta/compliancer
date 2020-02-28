fcbc9593e2b35fa557bbfb9e9af28f7f909d17cc
Bruno Henrique Costa <55752438+bhpcosta@users.noreply.github.com>
2020-02-28 08:54:19 -0300

    Update sw_cached_pages.js

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -1,1 +1,47 @@
+const cacheName = 'v1';
 
+const cacheAssets = [
+  'index.php',
+  'master.css',
+  '/img',
+  '/js/main.js'
+];
+
+// Call Install Event
+self.addEventListener('install', e => {
+  console.log('Service Worker: Instalado com sucesso!');
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log('Service Worker: Armazenando Arquivos em Cache');
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Call Activate Event
+self.addEventListener('activate', e => {
+  console.log('Service Worker: Ativado');
+  // Remove unwanted caches
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log('Service Worker: Limpando o cache antigo');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Call Fetch Event
+self.addEventListener('fetch', e => {
+  console.log('Service Worker: Fetching');
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
